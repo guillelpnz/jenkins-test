7e16d1335e92f1e488cba91a3fe025aee633fd51
Guillermo.Tapia <Guillermo.Tapia@DESKTOP-C16DE8O>
2022-09-20 12:20:49 +0200

    trigger build

--- src/java_test_package/TestMain.java
+++ src/java_test_package/TestMain.java
@@ -3,7 +3,7 @@ package java_test_package;
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test that triggers a Jenkins build");
+		System.out.println("This is a test that triggers a Jenkins buildd");
 	}
 
 }
